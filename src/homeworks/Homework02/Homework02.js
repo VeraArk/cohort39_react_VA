0ba9f8a5870be18c4ea3ca842625321f0ccd39db
VeraArk <veraarkind@gmail.com>
2024-06-28 20:58:49 +0300

    add

--- src/homeworks/Homework02/Homework02.js
+++ src/homeworks/Homework02/Homework02.js
@@ -1,12 +1,14 @@
 import Cards from "../../components/Cards/Cards";
-import "./styles.css"
+import "./styles.css";
 
-function Homework02(){
-return (<div className="homework02_page">
-    <Cards/>
-    <Cards/>
-    <Cards/>
-</div>)
+function Homework02() {
+  return (
+    <div className="homework02_page">
+      <Cards />
+      <Cards />
+      <Cards />
+    </div>
+  );
 }
 
 export default Homework02;
